71c9fe3e444e8bf6b989f3a9a43cb1c144153380
someone <someone>
2019-12-14 11:03:28 -0800

    built React app for babysitter kata

---
+++ babysitter-kata/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="jumbotron bg-white">
+        <h1>Babysitter Kata</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
